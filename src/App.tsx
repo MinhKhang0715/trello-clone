1090b7f068ec3ef8ca21279e6dd9750cd3c4f950
MinhKhang0715 <nkhang982@gmail.com>
2023-03-20 13:41:34 +0700

    Reafactor the code base

--- src/App.tsx
+++ src/App.tsx
@@ -1,141 +1,16 @@
+import React from 'react';
 import './App.scss';
-import logo from './logo.svg';
+
+import AppBar from './components/AppBar/AppBar';
+import BoardBar from './components/BoardBar/BoardBar';
+import BoardContent from './components/BoardContent/BoardContent';
 
 function App() {
   return (
     <div className="main-page">
-      <nav className="navbar app">App bar</nav>
-      <nav className="navbar board">Board bar</nav>
-      <div className="board-columns">
-        <div className="column">
-          <header>Brain storm</header>
-          <ul>
-            <li>
-              <img src={logo}
-                alt='logo' />
-              TITLE: THIS IS A LOGO
-            </li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brain storm</header>
-          <ul>
-            <li>
-              <img src={logo}
-                alt='logo' />
-              TITLE: THIS IS A LOGO
-            </li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brain storm</header>
-          <ul>
-            <li>
-              <img src={logo}
-                alt='logo' />
-              TITLE: THIS IS A LOGO
-            </li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brain storm</header>
-          <ul>
-            <li>
-              <img src={logo}
-                alt='logo' />
-              TITLE: THIS IS A LOGO
-            </li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brain storm</header>
-          <ul>
-            <li>
-              <img src={logo}
-                alt='logo' />
-              TITLE: THIS IS A LOGO
-            </li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brain storm</header>
-          <ul>
-            <li>
-              <img src={logo}
-                alt='logo' />
-              TITLE: THIS IS A LOGO
-            </li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brain storm</header>
-          <ul>
-            <li>
-              <img src={logo}
-                alt='logo' />
-              TITLE: THIS IS A LOGO
-            </li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brain storm</header>
-          <ul>
-            <li>
-              <img src={logo}
-                alt='logo' />
-              TITLE: THIS IS A LOGO
-            </li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-            <li>Add what you like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </div>
   );
 }
