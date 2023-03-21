16d13e3e124278d54f7a1e46a6d1f8ce9673dfe5
MinhKhang0715 <nkhang982@gmail.com>
2023-03-21 18:30:37 +0700

    Implement drag and drop

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import { Container, Draggable, DropResult } from "react-smooth-dnd";
 import './Column.scss'
-
 import Card from "../Card/Card";
 import { TrelloColumn } from "../../actions/interfaces";
 
@@ -10,12 +10,33 @@ function Column(props: TrelloColumn) {
     return column.cardOrder.indexOf(a.id) - column.cardOrder.indexOf(b.id);
   });
 
+  const onCardDrop = (dropResult: DropResult) => {
+    console.log(dropResult);
+  }
+
   return (
     <div className="column">
-      <header>{column.title}</header>
-      <ul className="card-list">
-        {cards.map((card, index) => <Card key={index} {...card} />)}
-      </ul>
+      <header className="column-drag-handle">{column.title}</header>
+      <div className="card-list">
+        <Container
+          groupName="trello-columns"
+          onDrop={onCardDrop}
+          getChildPayload={index => cards[index]}
+          dragClass="card-ghost"
+          dropClass="card-ghost-drop"
+          dropPlaceholder={{
+            animationDuration: 150,
+            showOnTop: true,
+            className: 'card-drop-preview'
+          }}
+        >
+          {cards.map((card, index) => (
+            <Draggable key={index}>
+              <Card {...card} />
+            </Draggable>
+          ))}
+        </Container>
+      </div>
       <footer>Add another card</footer>
     </div>
   );
